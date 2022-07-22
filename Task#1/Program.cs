68e5d9493917692a3798ef7262638b98b98c8c9b
«Dmitrii <Ambal003@ya.ry>
2022-07-22 19:05:23 +0300

    Сделал 2е задание

--- Task#1/Program.cs
+++ Task#1/Program.cs
@@ -1,4 +1,4 @@
-﻿// Если бы задача была бы без написания цикла, то программа выглядила бы так 
+﻿// Если бы задача была без написания цикла, то программа выглядила бы так: 
 
 //Console.WriteLine("Введите чило");
 //int num = int.Parse(Console.ReadLine());
@@ -9,15 +9,14 @@
 // Напишите цикл, который принимает на вход два числа (A и B)
 // и возводит число A в натуральную степень B.
 Console.WriteLine("Введите чило");
-int num = int.Parse(Console.ReadLine());    //3
+int num = int.Parse(Console.ReadLine());    
 Console.WriteLine("Введите степень");
-int deg = int.Parse(Console.ReadLine());    //5
+int deg = int.Parse(Console.ReadLine());    
 int res = num;
-for (int i = 2; i < deg + 1; i++)        //i=2  2<5 
-{
-    int n = res * num;               //  9 = 3*3
-    res = num * res; 
-    Console.WriteLine(n);                 
+for (int i = 2; i < deg + 1; i++)        
+{   
+    int n = res * num;               
+    res = num * res;                 
 }
 
  Console.WriteLine($"Результат: {res}");
---
+++ Task#2/Program.cs
@@ -0,0 +1,21 @@
+﻿
+int sumEnd(int inum , int l) 
+{
+int sum = 0;
+for (int i = l; i > 0; i--)
+{
+    int discharge = inum;
+    discharge = discharge % 10;
+    sum = sum + discharge;
+    inum = inum / 10;
+}
+return sum;
+}
+
+Console.WriteLine("Введите число");
+string num = Console.ReadLine();
+int len1 = num.Length;
+int inum = int.Parse(num);
+
+Console.WriteLine(sumEnd( inum , len1));
+
